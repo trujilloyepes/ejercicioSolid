d636e78b91999608f5b302d4d8f513af6d095dce
trujilloyepes <trujilloyepes19@gmail.com>
2026-03-20 14:51:32 +0100

    Update GeneradorTextoMensajes.java

--- src/main/java/mensajes/GeneradorTextoMensajes.java
+++ src/main/java/mensajes/GeneradorTextoMensajes.java
@@ -1,29 +1,11 @@
+Ahora el generador es una sola línea. No importa cuántos mensajes nuevos crees, esta clase nunca más se toca.
+
+Java
 package mensajes;
 
 public class GeneradorTextoMensajes {
-
     public String generar(Mensaje msg) {
         String asuntoBase = "[" + msg.getCanal().toUpperCase() + "] ";
-
-        if (msg.getTipo() == TipoMensaje.AVISO) {
-            return asuntoBase + "Aviso: " + msg.getTitulo() + "\n" +
-                   "Hola " + msg.getDestinatario() + ",\n" +
-                   msg.getCuerpo() + "\n" +
-                   "Fecha: " + msg.getFecha() + "\n";
-        }
-
-        if (msg.getTipo() == TipoMensaje.INCIDENCIA) {
-            return asuntoBase + "Incidencia (" + msg.getGravedad() + "): " + msg.getTitulo() + "\n" +
-                   "Alumno/a: " + msg.getAlumno() + "\n" +
-                   "Tutoría: " + msg.getTutoria() + "\n" +
-                   "Detalle: " + msg.getCuerpo() + "\n";
-        }
-
-        if (msg.getTipo() == TipoMensaje.FELICITACION) {
-            return asuntoBase + "¡Enhorabuena! " + msg.getTitulo() + "\n" +
-                   msg.getDestinatario() + ", " + msg.getCuerpo() + "\n";
-        }
-
-        return asuntoBase + msg.getTitulo() + "\n" + msg.getCuerpo();
+        return asuntoBase + msg.generarContenido();
     }
 }
