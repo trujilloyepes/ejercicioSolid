ebbd78183695f56bf9c9ccd1afd447692296b510
trujilloyepes <trujilloyepes19@gmail.com>
2026-03-20 14:50:42 +0100

    Update App.java

--- src/main/java/matricula/App.java
+++ src/main/java/matricula/App.java
@@ -4,8 +4,10 @@ public class App {
 
     public static void main(String[] args) {
         MatriculaService servicio = new MatriculaService();
+
         servicio.crearMatricula("12345678A", "DAM1", 300, true);
         servicio.crearMatricula("87654321B", "SMR2", 200, false);
+
         System.out.println(servicio.exportarResumen());
     }
 }
