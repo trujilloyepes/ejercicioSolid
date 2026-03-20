0db21d097072bb9201709ac532f00c3bcd09370b
trujilloyepes <trujilloyepes19@gmail.com>
2026-03-20 14:43:17 +0100

    Update Mensaje.java

--- src/main/java/mensajes/Mensaje.java
+++ src/main/java/mensajes/Mensaje.java
@@ -1,47 +1,21 @@
 package mensajes;
 
-public class Mensaje {
+public abstract class Mensaje {
+    protected final String canal;
+    protected final String destinatario;
+    protected final String titulo;
+    protected final String cuerpo;
 
-    private final TipoMensaje tipo;
-    private final String canal;
-    private final String destinatario;
-    private final String titulo;
-    private final String cuerpo;
-    private final String fecha;
-
-    // Atributos extra para indicencias
-    private final String alumno;
-    private final String tutoria;
-    private final String gravedad;
-
-    public Mensaje(TipoMensaje tipo,
-                   String canal,
-                   String destinatario,
-                   String titulo,
-                   String cuerpo,
-                   String fecha,
-                   String alumno,
-                   String tutoria,
-                   String gravedad) {
-        this.tipo = tipo;
+    public Mensaje(String canal, String destinatario, String titulo, String cuerpo) {
         this.canal = canal;
         this.destinatario = destinatario;
         this.titulo = titulo;
         this.cuerpo = cuerpo;
-        this.fecha = fecha;
-        this.alumno = alumno;
-        this.tutoria = tutoria;
-        this.gravedad = gravedad;
     }
 
-    public TipoMensaje getTipo() { return tipo; }
-    public String getCanal() { return canal; }
-    public String getDestinatario() { return destinatario; }
-    public String getTitulo() { return titulo; }
-    public String getCuerpo() { return cuerpo; }
-    public String getFecha() { return fecha; }
+    public abstract String formatear();
 
-    public String getAlumno() { return alumno; }
-    public String getTutoria() { return tutoria; }
-    public String getGravedad() { return gravedad; }
+    protected String getCabecera() {
+        return "[" + canal.toUpperCase() + "] ";
+    }
 }
