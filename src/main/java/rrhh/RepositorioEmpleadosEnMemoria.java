32530f1d2d49c26c974abe0454c2761e0552277a
trujilloyepes <trujilloyepes19@gmail.com>
2026-03-20 14:52:11 +0100

    Update RepositorioEmpleadosEnMemoria.java

--- src/main/java/rrhh/RepositorioEmpleadosEnMemoria.java
+++ src/main/java/rrhh/RepositorioEmpleadosEnMemoria.java
@@ -3,14 +3,15 @@ package rrhh;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RepositorioEmpleadosEnMemoria {
-
+public class RepositorioEmpleadosEnMemoria implements EmpleadoRepository {
     private final Map<String, Empleado> bd = new HashMap<>();
 
+    @Override
     public boolean existe(String dni) {
         return bd.containsKey(dni);
     }
 
+    @Override
     public void guardar(Empleado e) {
         bd.put(e.getDni(), e);
     }
